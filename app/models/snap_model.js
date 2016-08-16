07545b8dbe3c7c9187e06b051d96fb9253acbb0e
Rajiv Ramaiah <rajivramaiah@me.com>
2016-08-15 20:25:15 -0400

    added models

---
+++ app/models/snap_model.js
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from 'mongoose';
+
+// create a schema for posts with a field
+const SnapSchema = new Schema({
+  picture: String,
+});
+
+// create model class
+const SnapModel = mongoose.model('User', SnapSchema);
+
+export default SnapModel;
